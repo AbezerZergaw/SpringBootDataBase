20f395fab562a7e451d0ac6a6f3088ee75c5680c
GBTC440013UR <GBTC440013UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-24 17:05:49 -0400

    Initial commit

---
+++ src/main/java/com/example/springbootwithdatabasse/JobRepository.java
@@ -0,0 +1,9 @@
+package com.example.springbootwithdatabasse;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface JobRepository  extends CrudRepository<Job, Long>{
+
+
+
+}
---
+++ src/test/java/com/example/springbootwithdatabasse/SpringbootwithdatabasseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springbootwithdatabasse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootwithdatabasseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
